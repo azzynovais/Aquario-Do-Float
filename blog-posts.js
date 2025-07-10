3a90422c02eb15bb8915d5dd37503af66f3b22dd
azzynovais <floatingfloatskies@protonmail.com>
2025-07-10 14:34:56 -0400

    Started initrd

---
+++ blog-posts.js
@@ -0,0 +1,35 @@
+// blog-posts.js
+const blogPosts = [
+    {
+        title: "Newsletter do dia 10/07/2025:",
+        author: "Azzy Novais",
+        date: "10 de Julho de 2025",
+        summary: "A Newsletter de hoje, com novidades do mundo open-source.",
+        image: "./img/newsletter10072025.jpg",
+        file: "posts/post-newsletter-10-07-2025.html" 
+    },
+{
+    title: "Curiosidade: PhotoGIMP, um PhotoShop Open Source com base GIMP",
+    author: "Azzy Novais",
+    date: "10 de Julho de 2025",
+    summary: "Um software de edição de imagens open-source que se assemelha ao Adobe Photoshop",
+    image: "./img/photogimp.png",
+    file: "/posts/post-photogimp.html"
+},
+{
+    title: "Ubuntu 24.10 já está com seu fim de suporte",
+    author: "Azzy Novais",
+    date: "10 de julho de 2025",
+    summary: "O Ubuntu 24.10, codinome, Oracular Oriole, tem seu suporte cessado, usuarios devem migrar para a ultima release e/ou usar a LTS.",
+    image: "./img/ubuntuoracularoriole.png",
+    file: "posts/post-fim-de-suporte-oracular-oriolee.html"
+},
+{
+    title: "EndeavourOS: A Distribuição Linux que Facilita a Vida dos Usuários Intermediario",
+    author: "Azzy Novais",
+    date: "10 de julho de 2025",
+    summary: "O EndeavourOS é uma distro baseado no Arch, venha conhecer um pouco.",
+    image: "./img/endeavouros.jpeg",
+    file: "posts/post-endeavouros.html"
+},
+];
